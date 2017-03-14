5ff61e8a8d3f355e8cd39e27e334b35d62eb159d
Hilton Shumway <hillshum@gmail.com>
2017-03-13 23:13:07 -0600

    Add basic optimistic loading

--- src/store/actions/message.js
+++ src/store/actions/message.js
@@ -1,6 +1,8 @@
 import * as types from '../types'
 import * as server from '../server'
 
+import store from '../'
+
 export const listen = ()=>(dispatch)=>{
   server.listen((msg)=>{
     dispatch({
@@ -11,5 +13,15 @@ export const listen = ()=>(dispatch)=>{
 }
 
 export const sendMessage = (data, type, cb)=>()=>{
-  server.sendMessage(data, type).then(cb)
-}
+  data.optimistKey = Math.round(Math.random() * 5000)
+  const user = {
+    email: 'hillshum@gmail.com',
+    username: 'TODO'
+  }
+  store.dispatch({
+    type: types.ADD_MESSAGE,
+    payload: {type: 'text', data, user, updatedAt: new Date().toISOString()}
+  })
+  server.sendMessage(data, type)
+  if (typeof cb === 'function') cb(args)
+  }
--- src/store/reducer/message.js
+++ src/store/reducer/message.js
@@ -6,6 +6,10 @@ const initialState = []
 export default function(state=initialState, action){
   const actions = {
     [types.ADD_MESSAGE]: ()=>{
+      state = state.filter(item => {
+        if (!action.payload.data.optimistKey) return true
+        return item.data.optimistKey !== action.payload.data.optimistKey
+      })
       return [...state, action.payload]
     }
   }
